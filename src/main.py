1e9302cf8186ada4348e10e9ac11717885460bb4
Monica <monicaa2@umbc.edu>
2026-05-20 18:06:46 -0400

    added theme json file

--- src/main.py
+++ src/main.py
@@ -8,6 +8,8 @@ root = ctk.CTk()
 root.title("MoniMode")
 root.geometry("500x500+900+300")
 
+ctk.set_default_color_theme("theme.json")
+
 #this allows for the window stretching
 root.grid_rowconfigure(0, weight=1)
 root.columnconfigure(0, weight=1)
@@ -21,19 +23,19 @@ main_menu_frame.grid(row=0, column=0, sticky="nsew")
 #pack stuff onto start frame
 showFrame(start_frame)
 startLabel = ctk.CTkLabel(start_frame, text="Welcome to moniMode!!!! :3")
-startLabel.pack()
+startLabel.pack(pady=70)
 startButton = ctk.CTkButton(start_frame, command=main_menu_frame.tkraise, text="Click this to start!")
-startButton.pack()
+startButton.pack(pady=60)
 
 #pack stuff on main menu 
 menuLabel = ctk.CTkLabel(main_menu_frame, text="MAIN MENU\n")
-menuLabel.pack()
+menuLabel.pack(pady=60)
 calcButton = ctk.CTkButton(main_menu_frame, text="♡  CALCULATOR ♡")
-calcButton.pack()
+calcButton.pack(pady=30)
 todoButton = ctk.CTkButton(main_menu_frame, text="♡  TO-DO LIST  ♡")
-todoButton.pack()
+todoButton.pack(pady=30)
 focusButton = ctk.CTkButton(main_menu_frame, text="♡  FOCUS TIMER  ♡" )
-focusButton.pack()
+focusButton.pack(pady=30)
 
 
 
